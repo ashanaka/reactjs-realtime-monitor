828c5a06d3b569f77e438a4800160d44793949a7
ashanaka <jpashanshanaka@gmail.com>
2021-01-10 16:30:50 +0530

    data recieves realtime

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Line} from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from 'firebase';
+import {DB_CONFIG} from './Config';
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    if (!firebase.apps.length) {
+        this.app = firebase.initializeApp(DB_CONFIG);
+    }else {
+        this.app = firebase.app(); // if already initialized, use that one
+    }
+
+    // this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('Machine1');
+
+    this.state = {
+      input: 10,
+      labels: ['January', 'February', 'March',
+        'April', 'May'],
+      datasets: [
+        {
+          label: 'Rainfall',
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: [65, 59, 80, 81, 56]
+        }
+      ]
+    }
+
+  }
+
+  componentDidMount(){
+    this.database.on('value', snap => {
+        let vals = snap.val();
+        console.log(vals);
+        let valsArr = [];
+        let keysArr = [];
+        for (let [key, value] of Object.entries(vals)) {
+            // console.log(value);
+            valsArr.push(key.split(': '));
+            keysArr.push(value);
+        }
+
+        this.setState({
+            // input: snap.val(),
+            labels: valsArr,
+            datasets: [
+                {
+                    label: 'Rainfall',
+                    backgroundColor: 'rgba(75,192,192,1)',
+                    borderColor: 'rgba(0,0,0,1)',
+                    borderWidth: 2,
+                    data: keysArr
+                }
+            ]
+        });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, {this.state.input}</h1>
+          <Line
+              data={this.state}
+              options={{
+                  title:{
+                      display:true,
+                      text:'Average Rainfall per month',
+                      fontSize:20
+                  },
+                  legend:{
+                      display:true,
+                      position:'right'
+                  },
+                  maintainAspectRatio: true,
+                  redraw: true
+              }}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,10 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyC2Jv4hQSstFgLJfWhpW5GyrYd8MQaWlpg",
+    authDomain: "test-app-fece1.firebaseapp.com",
+    databaseURL: "https://test-app-fece1-default-rtdb.firebaseio.com",
+    projectId: "test-app-fece1",
+    storageBucket: "test-app-fece1.appspot.com",
+    messagingSenderId: "605583159352",
+    appId: "1:605583159352:web:c1b812b6a384bd699576cd",
+    measurementId: "G-NHBTZEX5PQ"
+}
